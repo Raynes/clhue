a1ea95e0ce56fb812a3ef4a3f8c429614c70b47d
Anthony Grimes <i@raynes.me>
2014-03-16 14:50:53 -0700

    Start on lights API.
    Scan for new lights, check existing lights, check for new lights.

---
+++ src/me/raynes/clhue/lights.clj
@@ -0,0 +1,19 @@
+(ns me.raynes.clhue.lights
+  (:require [me.raynes.clhue.request :refer [req]]))
+
+(defn lights
+  "Get ALL the lights."
+  [config]
+  (:body (req :get "lights" config)))
+
+(defn scan
+  "Scan for new lights."
+  [config]
+  (-> (req :post "lights" config)
+      :body
+      first))
+
+(defn new-lights
+  "Get lights found on last scan."
+  [config]
+  (:body (req :get "lights/new" config)))
