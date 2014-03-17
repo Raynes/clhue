cdf3be1f60585355934922e481706072fd392b9b
Anthony Grimes <i@raynes.me>
2014-03-16 17:53:52 -0700

    me.raynes/clhue 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clhue "0.1.1"
+(defproject me.raynes/clhue "0.1.2"
   :description "An API client for Hue."
   :url "https://github.com/Raynes/clhue"
   :license {:name "Eclipse Public License"
